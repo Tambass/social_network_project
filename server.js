0dd56da97709f92e3984b4603f07bcaa4e246e5d
Tambass <tambass85@gmail.com>
2020-07-31 09:47:47 +0200

    add users.js in controllers

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const http = require("http");
 const container = require("./container");
 const { Router } = require("express");
 
-container.resolve(function () {
+container.resolve(function (users) {
   const app = SetupExpress();
 
   function SetupExpress() {
@@ -17,4 +17,10 @@ container.resolve(function () {
       console.log("Écoute le port 3000");
     });
   }
+
+  //Setup router
+  const router = require("express-promise-router")();
+  users.SetRouting(router);
+
+  app.use(router);
 });
