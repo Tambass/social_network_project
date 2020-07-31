cc5f8ad511b957fca3dfbd8d38377984e9979a68
Tambass <tambass85@gmail.com>
2020-07-31 09:32:50 +0200

    require dependencies in server.js + connect server in port 3000

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 // Import dependencies
 
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const http = require("http");
+const container = require("./container");
+
+container.resolve(function () {
+  const app = SetupExpress();
+
+  function SetupExpress() {
+    const app = express();
+    const server = http.createServer(app);
+    server.listen(3000, function () {
+      console.log("Écoute le port 3000");
+    });
+  }
+});
